d46fb6881cf13404a1ce3a870c7b379a7efedcb3
realestindigo <jLoupias@roc-dev.com>
2019-05-07 09:14:40 +0100

    commit

---
+++ mijnjavascript.js
@@ -0,0 +1,12 @@
+document.getElementById('div1').style.backgroundColor = 'lightblue';
+document.getElementById('div2').style.backgroundColor = 'green';
+document.getElementById('div3').style.backgroundColor = 'yellow';
+document.getElementById('div4').style.backgroundColor = 'red';
+
+document.getElementById('div1').style.textAlign = 'center';
+document.getElementById('div2').style.textAlign = 'center';
+document.getElementById('div3').style.textAlign = 'right';
+document.getElementById('div4').style.textAlign = 'left';
+
+document.getElementById('div2').style.marginTop = '30px';
+document.getElementById('div3').style.marginTop = '40px';
